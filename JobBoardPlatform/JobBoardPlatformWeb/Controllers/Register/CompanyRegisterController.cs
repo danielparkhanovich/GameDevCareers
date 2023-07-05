945695b03c621897de8c866a6d2f394ed67e92f2
Daniel Parkhanovich <daniel.parkhanovich@gmail.com>
2023-07-05 21:24:37 +0200

    Added teches dropdown. Fixed multiselect employment types field.

--- JobBoardPlatform/JobBoardPlatformWeb/Controllers/Register/CompanyRegisterController.cs
+++ JobBoardPlatform/JobBoardPlatformWeb/Controllers/Register/CompanyRegisterController.cs
@@ -2,13 +2,17 @@
 using JobBoardPlatform.DAL.Models.Company;
 using JobBoardPlatform.PL.Interactors.Registration;
 using JobBoardPlatform.PL.ViewModels.Models.Authentification;
+using JobBoardPlatform.PL.ViewModels.Models.Offer.Company;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JobBoardPlatform.PL.Controllers.Register
 {
-    [Route("register-company")]
+    [Route("register-employer")]
     public class CompanyRegisterController : BaseRegisterController<CompanyRegisterViewModel>
     {
+        public const string AdsPricingAction = "RegisterPromotion";
+        public const string StartPostOfferAndRegisterAction = "StartPostOfferAndRegister";
+
         private readonly IRegistrationInteractor<CompanyRegisterViewModel> registrationInteractor;
         private readonly ILoginService<CompanyIdentity, CompanyProfile> loginService;
 
@@ -27,6 +31,41 @@ namespace JobBoardPlatform.PL.Controllers.Register
             return View();
         }
 
+        [Route("post-ad")]
+        public IActionResult StartPostOfferAndRegister()
+        {
+            var viewModel = new OfferDetailsViewModel();
+            return View(viewModel);
+        }
+
+        [Route("post-ad")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> StartPostOfferAndRegister(CompanyRegisterViewModel userRegister)
+        {
+            if (ModelState.IsValid)
+            {
+                var redirect = await registrationInteractor.ProcessRegistrationAndRedirect(userRegister);
+                return RedirectToAction(redirect.ActionName, redirect.Data);
+            }
+
+            return View(userRegister);
+        }
+
+        [Route("post-ad/confirm/{tokenId}")]
+        public IActionResult TryConfirmEmployer(string tokenId)
+        {
+            return View();
+        }
+
+        [Route("post-ad/confirm/{tokenId}")]
+        public async Task<IActionResult> TryConfirmOfferPaymentAndRegister(string tokenId)
+        {
+            await registrationInteractor.FinishRegistration(tokenId, HttpContext);
+            return View();
+        }
+
+        //////
         public override async Task<IActionResult> Register(CompanyRegisterViewModel userRegister)
         {
             if (ModelState.IsValid)
