186f84f7a75a6e1203f8512eefaa53333cca7e7a
Daniel Parkhanovich <daniel.parkhanovich@gmail.com>
2023-11-18 00:54:08 +0100

    Fixed offer data inserting.

--- JobBoardPlatform/JobBoardPlatform.BLL/Generators/MockOfferData.cs
+++ JobBoardPlatform/JobBoardPlatform.BLL/Generators/MockOfferData.cs
@@ -4,25 +4,6 @@ namespace JobBoardPlatform.BLL.Generators
 {
     internal class MockOfferData : OfferData
     {
-        public int OfferId { get; set; }
-        public int PlanId { get; set; }
-        public string JobTitle { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
-        public int WorkLocationType { get; set; }
-        public string JobDescription { get; set; }
-        public int ApplicationsContactType { get; set; }
-        public string? ApplicationsContactEmail { get; set; }
-        public int MainTechnologyType { get; set; }
-        public EmploymentType[] EmploymentTypes { get; set; }
-        public string[]? TechKeywords { get; set; }
-        public string? Street { get; set; }
-        public string? ApplicationsContactExternalFormUrl { get; set; }
-        public string InformationClause { get; set; }
-        public bool IsDisplayConsentForFutureRecruitment { get; set; }
-        public string? ConsentForFutureRecruitmentContent { get; set; }
-        public bool IsDisplayCustomConsent { get; set; }
-        public string? CustomConsentTitle { get; set; }
-        public string? CustomConsentContent { get; set; }
+
     }
 }
